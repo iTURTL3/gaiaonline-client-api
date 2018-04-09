b1b32b26e7b2fec09b8e79d2686f2e7592807efe
jakemadness <j4kem4dness@live.com>
2018-04-09 06:28:37 +0100

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-var password = 'myAccountPassword';
-window.api   = window.gaiaonlineApi(password);
-
-// create trade with jeff
-window.api.createTrade('jeff', function() {
-   console.log('trade created with jeff!');
-}, function() {
-   console.log('unable to create trade with jeff!');
-});
