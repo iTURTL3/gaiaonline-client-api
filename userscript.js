07987194d74782d919160568ea31f0f896f18e07
jakemadness <j4kem4dness@live.com>
2018-04-09 07:16:55 +0100

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -6,8 +6,8 @@
 // @author       jakemadness
 // @license      GPL
 // @match        *://www.gaiaonline.com/*
-// @require      https://cdn.rawgit.com/jakemadness/gaiaonline-api/master/gaiaonline.api.js
-// @require      https://cdn.rawgit.com/jakemadness/gaiaonline-api/master/userscript.modifications.js
+// @require      https://cdn.rawgit.com/jakemadness/gaiaonline-api/2f69a11184bf31cf85f7bf85d148298ca9426d22/gaiaonline.api.js
+// @require      https://cdn.rawgit.com/jakemadness/gaiaonline-api/541bce8ee2383f39ed3826c187a820d80b43007a/userscript.modifications.js
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
